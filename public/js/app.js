68adb1a4f4eb2aa18b17d0b830bf16dd2f33030f
vasilvalkov <vasil.valkov@gmail.com>
2017-05-10 08:46:10 +0300

    Create app project

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var weatherApp = angular.module('weatherApp', [
+  // 'ngRoute',
+  // 'weatherApp.view1',
+  // 'weatherApp.view2',
+  // 'weatherApp.version'
+]);
+// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+//   $locationProvider.hashPrefix('!');
+
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
---
+++ public/js/controllers/WeatherInfoController.js
@@ -0,0 +1,6 @@
+'use strict';
+
+weatherApp.controller('WeatherInfoController', 
+    function($scope) {
+        $scope.city = "Sofia";
+    });
