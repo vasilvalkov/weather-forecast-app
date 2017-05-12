01a5277571ffd54dc17f4e347596b57a53ba4e44
vasilvalkov <vasil.valkov@gmail.com>
2017-05-12 16:31:46 +0300

    Implemented search for a city, redirect to forecast page or notify user if error

--- public/js/controllers/WeatherInfoController.js
+++ public/js/controllers/WeatherInfoController.js
@@ -2,16 +2,30 @@
 
 weatherApp.controller('WeatherInfoController',
     function ($scope, forecastData) {
-        forecastData.getForecast('Sofia', 2)
-            .then(function (result) {
-                $scope.location = result.data.location;
-                $scope.current = result.data.current;
-                $scope.forecast = result.data.forecast;
-            },
-            function (error) {
-                // TODO: notify user no data for the entered city, town or village
-            });
+        $scope.formData = {
+            cityName: ''
+        };
+        
+        $scope.infoText = {
+            className: 'text-default',
+            message: 'Enter name of a city and press search:'
+        };
 
-            $scope.content = 'today';
+        $scope.getData = function () {
+            forecastData.getForecast($scope.formData.cityName, 2)
+                .then(function (result) {
+                        $scope.location = result.data.location;
+                        $scope.current = result.data.current;
+                        $scope.forecast = result.data.forecast;
+                    },
+                    function (error) {
+                        $scope.infoText.className = 'text-warning';
+                        $scope.infoText.message = error.data.error.message;
+                    })
+                .catch(function (ex) {
+                    console.log(ex.message);
+                });
+        }
 
+        $scope.content = 'today';
     });
--- public/js/services/forecastData.js
+++ public/js/services/forecastData.js
@@ -1,10 +1,14 @@
 weatherApp.factory('forecastData', function ($http) {
     return {
         getForecast: function (cityName, daysCount) {
-            const url = 'http://api.apixu.com/v1/forecast.json?key=29d83fa2298a47d29bb121845161212&q=' +
-                cityName + '&days=' + daysCount;
-
-           return $http.get(url);
+            
+            return $http.get('http://api.apixu.com/v1/forecast.json', {
+                params: {
+                    key: '29d83fa2298a47d29bb121845161212',
+                    q: cityName,
+                    days: daysCount
+                }
+            });
         }
     }
 })
